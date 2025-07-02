7c5844accd7409cdb918eb4858e88d033bfd7695
Mirva Dudhagara <middudha@iu.edu>
2025-07-02 17:38:31 -0400

    Initial Xcode project setup for AudioMind

---
+++ AudioMindTests/AudioMindTests.swift
@@ -0,0 +1,17 @@
+//
+//  AudioMindTests.swift
+//  AudioMindTests
+//
+//  Created by Mirvaben Dudhagara on 7/2/25.
+//
+
+import Testing
+@testable import AudioMind
+
+struct AudioMindTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
